95ee9e0196e15eaa05297641549287197a4f0304
tkwu <tkwu@ncsu.edu>
2023-12-16 02:15:49 -0500

    day 16 slightly cleaner

--- src/bin/16.rs
+++ src/bin/16.rs
@@ -18,21 +18,12 @@ enum Space {
 #[derive(Clone, Copy)]
 struct Tile {
     space: Space,
-    energized: u32,
     directions: u8,
 }
 
 #[derive(Clone, Copy)]
-enum Direction {
-    North,
-    South,
-    East,
-    West,
-}
-
-#[derive(Clone, Copy)]
 struct LightBeam {
-    direction: Direction,
+    direction: u8,
     x: usize,
     y: usize,
 }
@@ -52,7 +43,6 @@ fn parse_tile(input: &str) -> IResult<&str, Vec<Tile>> {
                     b'-' => Space::SplitterDash,
                     _ => unreachable!(),
                 },
-                energized: 0,
                 directions: 0,
             })
             .collect(),
@@ -63,253 +53,181 @@ fn parse_input(input: &str) -> IResult<&str, Vec<Vec<Tile>>> {
     separated_list1(newline, parse_tile)(input)
 }
 
+fn move_north(x: usize, y: usize) -> Option<LightBeam> {
+    if x == 0 {
+        None
+    } else {
+        Some(LightBeam {
+            direction: 0,
+            x: x - 1,
+            y,
+        })
+    }
+}
+
+fn move_south(x: usize, y: usize, rows: usize) -> Option<LightBeam> {
+    if x == rows - 1 {
+        None
+    } else {
+        Some(LightBeam {
+            direction: 1,
+            x: x + 1,
+            y,
+        })
+    }
+}
+
+fn move_east(x: usize, y: usize, cols: usize) -> Option<LightBeam> {
+    if y == cols - 1 {
+        None
+    } else {
+        Some(LightBeam {
+            direction: 2,
+            x,
+            y: y + 1,
+        })
+    }
+}
+
+fn move_west(x: usize, y: usize) -> Option<LightBeam> {
+    if y == 0 {
+        None
+    } else {
+        Some(LightBeam {
+            direction: 3,
+            x,
+            y: y - 1,
+        })
+    }
+}
+
 fn solve(mut map: Vec<Vec<Tile>>, start: LightBeam) -> u32 {
     let mut bfs_queue = VecDeque::from([start]);
 
     while let Some(LightBeam { direction, x, y }) = bfs_queue.pop_front() {
-        if map[x][y].energized > 0 {
-            match direction {
-                Direction::North => {
-                    if map[x][y].directions >> 0 & 1 == 1 {
-                        continue;
-                    }
-                }
-                Direction::South => {
-                    if map[x][y].directions >> 1 & 1 == 1 {
-                        continue;
-                    }
-                }
-                Direction::East => {
-                    if map[x][y].directions >> 2 & 1 == 1 {
-                        continue;
-                    }
-                }
-                Direction::West => {
-                    if map[x][y].directions >> 3 & 1 == 1 {
-                        continue;
-                    }
-                }
-            }
+        if (map[x][y].directions >> direction) & 1 == 1 {
+            continue;
         }
 
-        map[x][y].energized += 1;
-        map[x][y].directions |= 1 << {
-            match direction {
-                Direction::North => 0,
-                Direction::South => 1,
-                Direction::East => 2,
-                Direction::West => 3,
-            }
-        };
+        map[x][y].directions |= 1 << direction;
 
         match map[x][y].space {
             Space::EmptySpace => match direction {
-                Direction::North => {
-                    if x == 0 {
-                        continue;
+                0 => {
+                    if let Some(light_beam) = move_north(x, y) {
+                        bfs_queue.push_back(light_beam);
                     }
-                    bfs_queue.push_back(LightBeam {
-                        direction,
-                        x: x - 1,
-                        y,
-                    });
                 }
-                Direction::South => {
-                    if x == map.len() - 1 {
-                        continue;
+                1 => {
+                    if let Some(light_beam) = move_south(x, y, map.len()) {
+                        bfs_queue.push_back(light_beam);
                     }
-                    bfs_queue.push_back(LightBeam {
-                        direction,
-                        x: x + 1,
-                        y,
-                    });
                 }
-                Direction::East => {
-                    if y == map[0].len() - 1 {
-                        continue;
+                2 => {
+                    if let Some(light_beam) = move_east(x, y, map[0].len()) {
+                        bfs_queue.push_back(light_beam);
                     }
-                    bfs_queue.push_back(LightBeam {
-                        direction,
-                        x,
-                        y: y + 1,
-                    });
                 }
-                Direction::West => {
-                    if y == 0 {
-                        continue;
+                3 => {
+                    if let Some(light_beam) = move_west(x, y) {
+                        bfs_queue.push_back(light_beam);
                     }
-                    bfs_queue.push_back(LightBeam {
-                        direction,
-                        x,
-                        y: y - 1,
-                    });
                 }
+                _ => unreachable!(),
             },
             Space::MirrorForwardSlash => match direction {
-                Direction::North => {
-                    if y == map[0].len() - 1 {
-                        continue;
+                0 => {
+                    if let Some(light_beam) = move_east(x, y, map[0].len()) {
+                        bfs_queue.push_back(light_beam);
                     }
-                    bfs_queue.push_back(LightBeam {
-                        direction: Direction::East,
-                        x,
-                        y: y + 1,
-                    });
                 }
-                Direction::South => {
-                    if y == 0 {
-                        continue;
+                1 => {
+                    if let Some(light_beam) = move_west(x, y) {
+                        bfs_queue.push_back(light_beam);
                     }
-                    bfs_queue.push_back(LightBeam {
-                        direction: Direction::West,
-                        x,
-                        y: y - 1,
-                    });
                 }
-                Direction::East => {
-                    if x == 0 {
-                        continue;
+                2 => {
+                    if let Some(light_beam) = move_north(x, y) {
+                        bfs_queue.push_back(light_beam);
                     }
-                    bfs_queue.push_back(LightBeam {
-                        direction: Direction::North,
-                        x: x - 1,
-                        y,
-                    });
                 }
-                Direction::West => {
-                    if x == map.len() - 1 {
-                        continue;
+                3 => {
+                    if let Some(light_beam) = move_south(x, y, map.len()) {
+                        bfs_queue.push_back(light_beam);
                     }
-                    bfs_queue.push_back(LightBeam {
-                        direction: Direction::South,
-                        x: x + 1,
-                        y,
-                    });
                 }
+                _ => unreachable!(),
             },
             Space::MirrorBackwardSlash => match direction {
-                Direction::North => {
-                    if y == 0 {
-                        continue;
+                0 => {
+                    if let Some(light_beam) = move_west(x, y) {
+                        bfs_queue.push_back(light_beam);
                     }
-                    bfs_queue.push_back(LightBeam {
-                        direction: Direction::West,
-                        x,
-                        y: y - 1,
-                    });
                 }
-                Direction::South => {
-                    if y == map[0].len() - 1 {
-                        continue;
+                1 => {
+                    if let Some(light_beam) = move_east(x, y, map[0].len()) {
+                        bfs_queue.push_back(light_beam);
                     }
-                    bfs_queue.push_back(LightBeam {
-                        direction: Direction::East,
-                        x,
-                        y: y + 1,
-                    });
                 }
-                Direction::East => {
-                    if x == map.len() - 1 {
-                        continue;
+                2 => {
+                    if let Some(light_beam) = move_south(x, y, map.len()) {
+                        bfs_queue.push_back(light_beam);
                     }
-                    bfs_queue.push_back(LightBeam {
-                        direction: Direction::South,
-                        x: x + 1,
-                        y,
-                    });
                 }
-                Direction::West => {
-                    if x == 0 {
-                        continue;
+                3 => {
+                    if let Some(light_beam) = move_north(x, y) {
+                        bfs_queue.push_back(light_beam);
                     }
-                    bfs_queue.push_back(LightBeam {
-                        direction: Direction::North,
-                        x: x - 1,
-                        y,
-                    });
                 }
+                _ => unreachable!(),
             },
             Space::SplitterPipe => match direction {
-                Direction::North => {
-                    if x == 0 {
-                        continue;
-                    }
-                    bfs_queue.push_back(LightBeam {
-                        direction,
-                        x: x - 1,
-                        y,
-                    });
-                }
-                Direction::South => {
-                    if x == map.len() - 1 {
-                        continue;
-                    }
-                    bfs_queue.push_back(LightBeam {
-                        direction,
-                        x: x + 1,
-                        y,
-                    });
-                }
-                Direction::East | Direction::West => {
-                    if x > 0 {
-                        bfs_queue.push_back(LightBeam {
-                            direction: Direction::North,
-                            x: x - 1,
-                            y,
-                        });
-                    }
-                    if x < map.len() - 1 {
-                        bfs_queue.push_back(LightBeam {
-                            direction: Direction::South,
-                            x: x + 1,
-                            y,
-                        });
+                0 => {
+                    if let Some(light_beam) = move_north(x, y) {
+                        bfs_queue.push_back(light_beam);
                     }
                 }
+                1 => {
+                    if let Some(light_beam) = move_south(x, y, map.len()) {
+                        bfs_queue.push_back(light_beam);
+                    }
+                }
+                2 | 3 => {
+                    if let Some(light_beam) = move_north(x, y) {
+                        bfs_queue.push_back(light_beam);
+                    }
+                    if let Some(light_beam) = move_south(x, y, map.len()) {
+                        bfs_queue.push_back(light_beam);
+                    }
+                }
+                _ => unreachable!(),
             },
             Space::SplitterDash => match direction {
-                Direction::North | Direction::South => {
-                    if y < map[0].len() - 1 {
-                        bfs_queue.push_back(LightBeam {
-                            direction: Direction::East,
-                            x,
-                            y: y + 1,
-                        });
+                0 | 1 => {
+                    if let Some(light_beam) = move_east(x, y, map[0].len()) {
+                        bfs_queue.push_back(light_beam);
                     }
-                    if y > 0 {
-                        bfs_queue.push_back(LightBeam {
-                            direction: Direction::West,
-                            x,
-                            y: y - 1,
-                        });
+                    if let Some(light_beam) = move_west(x, y) {
+                        bfs_queue.push_back(light_beam);
                     }
                 }
-                Direction::East => {
-                    if y == map[0].len() - 1 {
-                        continue;
+                2 => {
+                    if let Some(light_beam) = move_east(x, y, map[0].len()) {
+                        bfs_queue.push_back(light_beam);
                     }
-                    bfs_queue.push_back(LightBeam {
-                        direction,
-                        x,
-                        y: y + 1,
-                    });
                 }
-                Direction::West => {
-                    if y == 0 {
-                        continue;
+                3 => {
+                    if let Some(light_beam) = move_west(x, y) {
+                        bfs_queue.push_back(light_beam);
                     }
-                    bfs_queue.push_back(LightBeam {
-                        direction,
-                        x,
-                        y: y - 1,
-                    });
                 }
+                _ => unreachable!(),
             },
         }
     }
 
     map.iter()
-        .map(|row| row.iter().filter(|space| space.energized > 0).count() as u32)
+        .map(|row| row.iter().filter(|space| space.directions != 0).count() as u32)
         .sum()
 }
 
@@ -317,7 +235,7 @@ pub fn part_one(input: &str) -> Option<u32> {
     let (_, map) = parse_input(input).unwrap();
 
     let start = LightBeam {
-        direction: Direction::East,
+        direction: 2,
         x: 0,
         y: 0,
     };
@@ -327,27 +245,27 @@ pub fn part_one(input: &str) -> Option<u32> {
 pub fn part_two(input: &str) -> Option<u32> {
     let (_, map) = parse_input(input).unwrap();
 
-    let mut start_locations: Vec<LightBeam> = Vec::new();
+    let mut start_locations: Vec<LightBeam> = Vec::with_capacity(map.len() * 2 + map[0].len() * 2);
     for i in 0..map.len() {
         start_locations.push(LightBeam {
-            direction: Direction::East,
+            direction: 2,
             x: i,
             y: 0,
         });
         start_locations.push(LightBeam {
-            direction: Direction::West,
+            direction: 3,
             x: i,
             y: map[0].len() - 1,
         });
     }
     for j in 0..map[0].len() {
         start_locations.push(LightBeam {
-            direction: Direction::South,
+            direction: 1,
             x: 0,
             y: j,
         });
         start_locations.push(LightBeam {
-            direction: Direction::North,
+            direction: 0,
             x: map.len() - 1,
             y: j,
         });
